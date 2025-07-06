0b9c196e91408755f3e57fbdc00ffb6ebfc2c166
Sayantan <sayantanhalder43@gmail.com>
2025-07-06 15:22:00 +0530

    First commit

--- Day 21/BubbleSortDecOrder.cpp
+++ Day 21/BubbleSortDecOrder.cpp
@@ -28,11 +28,15 @@ int main()
     // Bubble Sort
     for (int i = n - 2; i >= 0; i--)
     {
+        bool swapped = 0;
         for (int j = 0; j <= i; j++)
         {
             if (arr[j] < arr[j + 1])
                 swap(arr[j], arr[j + 1]);
+            swapped = 1;
         }
+        if (swapped == 0)
+            break;
     }
 
     cout << "The elements of the array after bubble sorting: " << endl;
