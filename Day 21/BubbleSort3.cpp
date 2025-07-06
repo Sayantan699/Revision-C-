0c7e318a0b23896311d27f6ff4629245de6d6132
Sayantan <sayantanhalder43@gmail.com>
2025-07-06 15:42:14 +0530

    Modified

---
+++ Day 21/BubbleSort3.cpp
@@ -0,0 +1,51 @@
+// Bubble Sort Algorithm to sort the array of integers in increasing order
+// by taking the smallest number to first place by starting from the last.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    int arr[n];
+
+    cout << "Enter the elements in the array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The elements of the array are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    { // i decides the no of rounds
+        bool swapped = 0;
+        for (int j = i + 1; j >= 0; j--)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapped = 1;
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+
+        if (swapped == 0)
+            break;
+    }
+
+    cout << endl;
+    cout << "The elements of the array after bubble sorting are: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
